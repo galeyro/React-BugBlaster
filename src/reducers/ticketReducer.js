bcba47f73405abb476ead79ae9ab222777f4b59e
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-04-27 12:29:16 -0500

    Sorting tickets by priority

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -36,6 +36,12 @@ export default function ticketReducer(state, action) {
     case "CLEAR_EDITING_TICKET":
       return { ...state, editingTicket: null };
 
+    case "SET_SORTING":
+      return {
+        ...state,
+        sortPreference: action.payload,
+      };
+
     default:
       return state;
   }
